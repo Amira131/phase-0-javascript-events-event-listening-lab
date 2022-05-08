c45ab8c4e4c57b3499bae1a1a3d3fc94cae8b8a6
Amira de Moya <demoyaamira@gmail.com>
2022-05-08 17:45:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    //input.addEventListener('click', clickAlert);
+    // input.addEventListener('click', function(event){
+    //     alert('I was clicked!');
+    // } )
+    input.addEventListener('click', () => alert("dog") )
 }
+function clickAlert() {
+    alert('I was clicked!');
+    console.log("sup")
+}
+addingEventListener();
